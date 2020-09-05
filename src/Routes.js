1453cf34bcd4287e1bab8960b1aaa25f852d1d37
nih13 <niharika1397@gmail.com>
2020-09-05 18:14:01 +0530

    admin dasboard created

--- src/Routes.js
+++ src/Routes.js
@@ -7,7 +7,11 @@ import AdminRoute from "./auth/helper/AdminRoutes";
 import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
-
+import AddCategory from "./admin/AddCategory";
+import ManageCategories from './admin/ManageCategories';
+import AddProduct from './admin/AddProduct';
+import ManageProducts from './admin/ManageProducts';
+import UpdateProduct from './admin/UpdateProduct';
 
 
 const Routes= ()=>{
@@ -19,6 +23,11 @@ const Routes= ()=>{
             <Route path="/signin" exact component = {Signin}/>
             <PrivateRoute path="/user/dashboard" exact component = {UserDashBoard} />
             <AdminRoute path="/admin/dashboard" exact component = {AdminDashBoard} />
+            <AdminRoute path="/admin/create/category" exact component = {AddCategory} />
+            <AdminRoute path="/admin/categories" exact component = {ManageCategories} />
+            <AdminRoute path="/admin/create/product" exact component = {AddProduct} />
+            <AdminRoute path="/admin/products" exact component = {ManageProducts} />
+            <AdminRoute path="/admin/product/update/:productId" exact component = {UpdateProduct} />
 
         </Switch>
         </BrowserRouter>
